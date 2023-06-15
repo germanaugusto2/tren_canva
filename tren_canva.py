348bff78c16ba82708cbee8c318117d130e74e23
germanaugusto2 <germanordonez@colegioguanenta.edu.cogit config --global user.email germanordonez@colegioguanenta.edu.co>
2023-06-15 15:16:20 -0500

    si

--- tren_canva.py
+++ tren_canva.py
@@ -1,50 +1,55 @@
 from tkinter import *
 
-#-------------------
-# Variables globales
-#-------------------
-BASE = 460
-ALTURA = 220
+#Variables x, y
+X = 460
+Y = 460
 
-#-------------------
-# ventana principal
-#-------------------
-
+#Crear una ventana 
 ventana_principal = Tk()
-ventana_principal.title("Tren")
+ventana_principal.title("Graficas 2D")
 ventana_principal.resizable(False,False)
 ventana_principal.geometry("500x500")
 ventana_principal.config(bg="white")
- 
-#---------------------
-# frame de graficacion
-#---------------------
-frame_graficacion = Frame(ventana_principal)
-frame_graficacion.config(bg="red", width=480, height=240)
-frame_graficacion.place(x=10, y=10)
 
-# creacion canvas
-c = Canvas(frame_graficacion,width=BASE, height=ALTURA)
+
+#frame
+
+graf = Frame(ventana_principal)
+graf.config(width=480, height=480, bg="red")
+graf.place(x=10,y=10)
+
+# Canvas
+
+c = Canvas(graf,width=X, height=Y)
 c.config(bg="black")
 c.place(x=10,y=10)
-frame_controles = Frame(ventana_principal)
-frame_controles.config(bg="blue", width=480, height=230)
-frame_controles.place(x=10,y=260)
-
-# creacion tren
-rect_1 = c.create_rectangle(BASE/1.3,ALTURA/3,BASE/2.3,ALTURA/4, fill="gray67")
-rect_3 = c.create_rectangle(BASE/1.2,ALTURA/1.4,BASE/4,ALTURA/2, fill="gray67")
-circ_1 = c.create_oval(BASE/2-20,ALTURA/1.5,BASE/4,ALTURA, fill="saddle brown")
-circ_2 = c.create_oval(BASE/1.7,ALTURA/1.5,BASE/1.3,ALTURA, fill="saddle brown")
-rect_2 = c.create_rectangle(BASE/1.4-20,ALTURA/1.2+15,BASE/2-30,ALTURA/2+60, fill="bisque2")
-rect_4 = c.create_rectangle(BASE/1.5,ALTURA/3,BASE/1.7-20,ALTURA/1.7-20, fill="gray92")
-rect_5 = c.create_rectangle(BASE/4,ALTURA/5,BASE/3,ALTURA/2, fill="gray67")
-rect_6 = c.create_rectangle(BASE/1.4,ALTURA/3,BASE/1.5,ALTURA/2, fill="gray67")
-rect_7 = c.create_rectangle(BASE/1.8,ALTURA/3,BASE/2,ALTURA/2, fill="gray67")
 
 
+#Rectangulo del tren
+rect_1 = c.create_rectangle(X/2-100,Y/2+5,X/2+200,Y/2+180, fill="gold")
+rect_2 = c.create_rectangle(X/2+25,Y/2-130,X/2+180,Y/2+5, fill="cyan")
+rect_3 = c.create_rectangle(X/2+50,Y/2-105,X/2+155,Y/2-20, fill="coral1")
+rect_4 = c.create_rectangle(X/2+5,Y/2-180,X/2+200,Y/2-130, fill="green3")
+rect_5 = c.create_rectangle(X/2+26,Y/2-215,X/2+179,Y/2-180, fill="tan2")
+rect_6 = c.create_rectangle(X/2-80,Y/2-55,X/2-40,Y/2+5, fill="lawn green")
+rect_7 = c.create_rectangle(X/2-100,Y/2-95,X/2-20,Y/2-55, fill="brown3")
 
-#crear nombre
-texto_1 = c.create_text(BASE/2, ALTURA/1.5, anchor="center", tex="German", font=("Arial", 20, "bold"), fill="cyan4", activefill="green yellow")
-# CREAR VENTANA
+rect_8 = c.create_rectangle(X/2-125,Y/2+150,X/2-100,Y/2+20, fill="purple2")
+circ_1 = c.create_oval(X/2-205,Y/2+25,X/2-150,Y/2+150, fill="red")
+rect_9 = c.create_rectangle(X/2-125,Y/2+5,X/2-170,Y/2+170, fill="olive drab")
+
+# Circulos del tren
+circ_2 = c.create_oval(X/2+120,Y/2+105,X/2+230,Y/2+220, fill="sky blue")
+circ_3 = c.create_oval(X/2-0,Y/2+105,X/2+110,Y/2+220, fill="sky blue")
+circ_4 = c.create_oval(X/2-120,Y/2+105,X/2-10,Y/2+220, fill="sky blue")
+
+# Otros rectangulos
+rect_7 = c.create_rectangle(X/2-50,Y/2+145,X/2+40,Y/2+185, fill="OliveDrab1")
+rect_8 = c.create_rectangle(X/2+70,Y/2+145,X/2+170,Y/2+185, fill="OliveDrab1")
+
+# Cara
+cara_1 = c.create_oval(X/2+55,Y/2-115,X/2+149,Y/2-20, fill="yellow")
+
+# Nombre (text)
+text_1 = c.create_text(X/2+50,Y/2+80, anchor="center", text="German Augusto Ordoñez Gonzalez", font=("Arial", 18, "bold"), fill="azure2", activefill="red")
 ventana_principal.mainloop()
